c9d2b1fd9b7ce5e693adedb259abe99ffcc0a449
janisxyz <janis.schelling@bbzbl-it.ch>
2026-06-16 17:17:13 +0200

    Update default region to eu-central-2 (Zurich)

--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 variable "region" {
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-2"
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "allowed_ssh_cidr" {
-  default = "0.0.0.0/0"  # Change to your IP for security
+  description = "CIDR block allowed for SSH (restrict for security)"
+  type        = string
+  default     = "0.0.0.0/0"  # Change to your IP
+}
+
+variable "wireguard_port" {
+  description = "WireGuard UDP port"
+  type        = number
+  default     = 51820
 }
